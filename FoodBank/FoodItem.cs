45c9420c845e2234dfc4995de53a3108820c75fb
mspulsi <mspulsipher@gmail.com>
2026-01-21 11:30:47 -0700

    comments

--- FoodBank/FoodItem.cs
+++ FoodBank/FoodItem.cs
@@ -2,11 +2,13 @@ namespace FoodBank;
 
 public class FoodItem
 {
+    // food item attributes
     public string name = "";
     public string category = "";
     public int quantity = 0;
     public string expDate = "";
     
+    // constructor that assigns the attributes
     public FoodItem(string name, string category, int quantity, string expDate)
     {
         this.name = name;
--- FoodBank/Program.cs
+++ FoodBank/Program.cs
@@ -1,5 +1,6 @@
 ﻿using FoodBank;
 
+// instantiate needed list and input variable
 List<FoodItem> foodItems = new List<FoodItem>();
 string userInput = "";
 
@@ -7,6 +8,7 @@ Console.WriteLine("Welcome to FoodBank!");
 
 do
 {
+    // display a simple menu for user
     Console.WriteLine();
     Console.WriteLine("What would you like to do?");
     Console.WriteLine("a: add a food item");
@@ -15,8 +17,11 @@ do
     Console.WriteLine("d: exit program");
     userInput = Console.ReadLine();
     Console.Clear();
+    
+    // add item
     if (userInput == "a")
     {
+        // when a then prompt user for necessary info to make a new food item
         Console.WriteLine("Enter the name of the food item:");
         string name = Console.ReadLine();
         Console.WriteLine("Enter the category of the food item:");
@@ -33,16 +38,25 @@ do
         }
         Console.WriteLine("Enter the expiration date of the food item");
         string expDate = Console.ReadLine();
+        
+        // make a new item and add to the list
         FoodItem newItem = new FoodItem(name, category, quantity, expDate);
         foodItems.Add(newItem);
         Console.Clear();
         Console.WriteLine("New food item has been added.");
     } 
+    
+    // delete item
     else if (userInput == "b")
     {
+        // get the name of the item user wants to delete
         Console.WriteLine("Enter the name of the food item you want to delete:");
         string name = Console.ReadLine();
+        
+        // find it in the list
         int index = foodItems.FindIndex(x => x.name == name);
+        
+        // delete if it is there, else tell user it doesn't exist
         if (index != -1)
         {
             foodItems.RemoveAt(index);
@@ -53,6 +67,8 @@ do
             Console.WriteLine("No such food item in the list");
         }
     }
+    
+    // print out all items and their info from the list
     else if (userInput == "c")
     {
         foreach (FoodItem foodItem in foodItems)
@@ -60,10 +76,14 @@ do
             Console.WriteLine(foodItem.name + " - " + foodItem.category + " qty:" + foodItem.quantity + " exp:"  + foodItem.expDate);
         }
     }
+    
+    // exit the program
     else if (userInput == "d")
     {
         Console.WriteLine("Goodbye");
     }
+    
+    // message if any other input is given
     else
     {
          Console.WriteLine("Invalid input");
